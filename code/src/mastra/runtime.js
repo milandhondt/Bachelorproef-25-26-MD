bdf65f4f3ba0e165717e25e7ff9eb372975f8fb0
Milan Dhondt <milan.dhondt@student.hogent.be>
2026-04-12 16:17:27 +0200

    niet meer nodig

--- code/src/mastra/runtime.js
+++
@@ -1,6 +0,0 @@
-import { Mastra } from "@mastra/core/mastra";
-import { evaluationAgent } from "./agents/evaluation-agent.js";
-
-export const mastra = new Mastra({
-  agents: { evaluationAgent },
-});
